dba5dfe4c88dc522d429f6c1cc9b6dbc905891d9
Alcides Andrade <junioralcides1994@gmail.com>
2021-12-13 13:47:35 +0900

    implement command `add` to add a copyright notice to existing files

---
+++ cmd/add.go
@@ -0,0 +1,112 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/speckJ8/cpr/langs"
+	"github.com/speckJ8/cpr/licenses"
+	"github.com/speckJ8/cpr/utils"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command {
+    Use: "add",
+    Aliases: []string{ "a" },
+    Short: "Add a copyright notice to existing files.",
+    Long: "Add a copyright notice to existing files.",
+    Args: cobra.MinimumNArgs(1),
+    Run: addToFile,
+}
+
+func init() {
+    addCmd.Flags().StringVarP(&author, "author", "u", "", "Author name")
+    addCmd.Flags().StringVarP(&email, "email", "e", "", "Author email")
+    addCmd.Flags().StringVarP(&license, "license", "l", "", "The license to use")
+    addCmd.Flags().BoolVarP(&short, "short", "s", false, "Use a short license notice")
+    addCmd.PersistentFlags().BoolVarP(&all, "all", "a", false,
+        "Add the notice to all the files in the project.")
+    addCmd.PersistentFlags().StringSliceVarP(&extensions, "extensions", "x", []string{},
+        "Add the notice only to files with these extensions.")
+}
+
+func addToFile(cmd *cobra.Command, args []string) {
+    if author == "" {
+        author = utils.DetectAuthor()
+        if author == "" {
+            fmt.Println("author name not specified")
+            os.Exit(1)
+        }
+    }
+
+    if email == "" {
+        email = utils.DetectEmail()
+        if email == "" {
+            fmt.Println("email not specified")
+            os.Exit(1)
+        }
+    }
+
+    var li *licenses.License
+    if license == "" {
+        li = utils.DetectLicense()
+        if li == nil {
+            fmt.Println("license not specified")
+            os.Exit(1)
+        }
+    } else {
+        li = licenses.Licenses[license]
+        if li == nil {
+            fmt.Printf("unknown license `%s`\n", license)
+            os.Exit(1)
+        }
+    }
+
+    var licenseNotice string
+    if short {
+        licenseNotice = li.Simple
+    } else {
+        licenseNotice = li.Verbose
+    }
+    tmp, err := template.New("notice").Parse(licenseNotice)
+    if err != nil {
+        fmt.Printf("failed to load license `%s`: %s\n", license, err.Error())
+        os.Exit(1)
+    }
+
+    var notice bytes.Buffer
+    var templateArgs = licenses.LicenseNoticeTemplateArgs {
+        CurrentYear: fmt.Sprintf("%d", time.Now().UTC().Year()),
+        Author: author,
+        Email: email,
+    }
+    tmp.Execute(&notice, templateArgs)
+
+    var noticeString = notice.String()
+    for _, file := range args {
+        var nameParts = strings.Split(file, ".")
+        var extension = nameParts[len(nameParts) - 1]
+        lang, ok := langs.Langs[extension]
+        if !ok {
+            fmt.Printf("unknown language extension .%s\n", extension)
+            os.Exit(1)
+        }
+
+        contents, err := os.ReadFile(file)
+        if err != nil {
+            fmt.Println(err.Error())
+            os.Exit(1)
+        }
+
+        contents = append([]byte(lang.WrapInComments(noticeString)), contents...)
+        err = os.WriteFile(file, contents, os.ModePerm)
+        if err != nil {
+            fmt.Printf("failed to write to %s: %s\n", file, err.Error())
+            os.Exit(1)
+        }
+    }
+}
